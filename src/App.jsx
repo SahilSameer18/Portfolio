b3ad264273c34d2c0aff1f81a1ff4cd0ed930993
sahil sameer siddique <sahilsameer.dev18@gmail.com>
2026-06-06 17:51:46 +0530

    change folder name from pages to sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Hero from "./pages/Hero";
+import Hero from "./sections/Hero";
 import Navbar from "./components/Navbar";
 import SoftBackdrop from "./components/SoftBackdrop";
-import About from "./pages/About";
-import Skills from "./pages/Skills";
-import Project from "./pages/Project";
-import Education from "./pages/Education"
-import Contact from "./pages/Contact";
-import Footer from "./pages/Footer";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Project from "./sections/Project";
+import Education from "./sections/Education";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 function App() {
   return (
